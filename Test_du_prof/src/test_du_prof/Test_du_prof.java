1dbc4892759f69a3f8d2c55af4773e88dd040bfb
p1926975 <nathan.lefevre@etu.univ-lyon1.fr>
2020-11-20 12:24:05 +0100

    création classes et des objects Nath et Steph

---
+++ Test_du_prof/src/test_du_prof/Test_du_prof.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_du_prof;
+
+/**
+ *
+ * @author Maître
+ */
+public class Test_du_prof {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Membre tuteur = new Membre("LEROUX", "Stéphane", 200);
+        Membre unicorn = new Membre("LEFEVRE", "Nathan", 19);
+        
+        //Inserez un nouvel object a votre nom
+        
+        
+        
+        //rajouter le méthode toString() sur vous
+        System.out.println(tuteur.toString());
+        System.out.println(unicorn.toString());
+    }
+    
+}
