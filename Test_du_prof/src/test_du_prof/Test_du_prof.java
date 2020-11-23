633418631d41c43b05b6dbaf7ab5affb965c6b60
PEZIERE ANTONIN p1910081 <antonin.peziere@etu.univ-lyon1.fr>
2020-11-23 21:11:58 +0100

    Revert "Merge branch 'Antonin' into 'master'"
    This reverts merge request !5

--- Test_du_prof/src/test_du_prof/Test_du_prof.java
+++ Test_du_prof/src/test_du_prof/Test_du_prof.java
@@ -15,17 +15,18 @@ public class Test_du_prof {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        Membre Anto=new Membre("ME","here",11);
+        Membre tuteur = new Membre("LEROUX", "Stéphane", 200);
+        Membre unicorn = new Membre("LEFEVRE", "Nathan", 19);
+        Membre emolga=new Membre("BOUHANA","Coralie",21);
         
         //Inserez un nouvel object a votre nom
         
         
         
-
-        //rajouter le méthode toString() sur moi
-
-        System.out.println(Anto.toString);
+        //rajouter le méthode toString() sur vous
+        System.out.println(tuteur.toString());
+        System.out.println(unicorn.toString());
+        System.out.println(emolga);
 
     }
     
