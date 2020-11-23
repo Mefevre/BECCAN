cbc66b9ba246e2e27d74e420057d556c0e877616
Coralie BOUHANA <coraliebouhana@etu.univ-lyon1.fr>
2020-11-23 19:53:53 +0100

    Add Membre Coralie

--- Test_du_prof/src/test_du_prof/Test_du_prof.java
+++ Test_du_prof/src/test_du_prof/Test_du_prof.java
@@ -17,6 +17,7 @@ public class Test_du_prof {
     public static void main(String[] args) {
         Membre tuteur = new Membre("LEROUX", "Stéphane", 200);
         Membre unicorn = new Membre("LEFEVRE", "Nathan", 19);
+        Membre emolga=new Membre("BOUHANA","Coralie",21);
         
         //Inserez un nouvel object a votre nom
         
@@ -25,6 +26,8 @@ public class Test_du_prof {
         //rajouter le méthode toString() sur vous
         System.out.println(tuteur.toString());
         System.out.println(unicorn.toString());
+        System.out.println(emolga);
+
     }
     
 }
