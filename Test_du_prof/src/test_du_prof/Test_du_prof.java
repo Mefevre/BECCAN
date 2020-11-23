e4af8541b02cf2b60be7c2613cfadc375119237d
PEZIERE ANTONIN p1910081 <antonin.peziere@etu.univ-lyon1.fr>
2020-11-23 20:56:55 +0100

    ajout mon nom

---
+++ Test_du_prof/src/test_du_prof/Test_du_prof.java
@@ -0,0 +1,17 @@
+public class Test_du_prof {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Membre ANTO = new Membre("ME","HERE",20)
+        //Inserez un nouvel object a votre nom
+        
+        
+        
+	//Ajoute ma sout
+	System.out.printl(ANTO.toString());
+
+    }
+    
+}
