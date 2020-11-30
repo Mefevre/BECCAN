3dd8413a7cde12ca8cfd42a566a2101b9b5a33ef
PEZIERE ANTONIN p1910081 <antonin.peziere@etu.univ-lyon1.fr>
2020-11-30 13:31:35 +0100

    Update Test_du_prof.java

--- Test_du_prof/src/test_du_prof/Test_du_prof.java
+++ Test_du_prof/src/test_du_prof/Test_du_prof.java
@@ -18,13 +18,15 @@ public class Test_du_prof {
         Membre tuteur = new Membre("LEROUX", "Stéphane", 200);
         Membre unicorn = new Membre("LEFEVRE", "Nathan", 19);
         Membre test = new Membre("TEST", "Test", 600);
+        Membre Antonin = new Membre("ANtonin","me" , 45);
         
         //Inserez un nouvel object a votre nom
         
         
-        
+        System.out.println("Vous etes beau ! ");
         //rajouter le méthode toString() sur vous
         System.out.println(tuteur.toString());
+        System.out.println(Antonin.toString());
         System.out.println(unicorn.toString());
     }
     
