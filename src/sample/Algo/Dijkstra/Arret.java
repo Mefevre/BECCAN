31ccd87243eba1fcfd7e134f1dfae898823e8052
Nathan <lefevre.nathan21@gmail.com>
2021-02-11 16:32:44 +0100

    correction supp fichier

---
+++ src/sample/Algo/Dijkstra/Arret.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.Algo.Dijkstra;
+
+/**
+ *
+ * @author Me
+ */
+public class Arret {
+    private Sommet s1;
+    private Sommet s2;
+
+    public Arret(Sommet e, Sommet r) {
+        this.s1 = e;
+        this.s2 = r;
+    }
+
+    public Sommet get_1() {
+        return s1;
+    }
+
+    public Sommet get_2() {
+        return s2;
+    }
+
+    @Override
+    public int hashCode() {
+        return s1.getNumero() * 7 + s2.getNumero() * 5;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Arret arret = (Arret) obj;
+        if (
+                (this.s1.equals(arret.s1) && this.s2.equals(arret.s2)) ||
+                        (this.s1.equals(arret.s2) && this.s1.equals(arret.s2))
+
+        ) return true;
+
+
+        return false;
+    }
+}
---
+++ src/sample/Algo/Dsatur/Dsatur.java
@@ -0,0 +1,24 @@
+package sample.Algo.Dsatur;
+
+import sample.Algo.Dijkstra.Sommet;
+
+import java.util.LinkedList;
+
+public class Dsatur {
+    private int[][] matrice;
+    private LinkedList<Sommet> listSommets;
+
+    public Dsatur(int[][] mat, LinkedList<Sommet> lS) {
+        matrice = mat;
+        listSommets = lS;
+
+        executionAlgo();
+    }
+
+    private void executionAlgo()
+    {
+
+    }
+
+
+}
